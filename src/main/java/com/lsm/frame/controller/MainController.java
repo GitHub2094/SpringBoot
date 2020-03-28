83d0119a30f9983d368b0a6ac0054e4e4cbbfdf7
lsm <1094668269@qq.com>
2020-03-28 10:26:30 +0800

    Mybatis配置

--- src/main/java/com/lsm/frame/controller/MainController.java
+++ src/main/java/com/lsm/frame/controller/MainController.java
@@ -1,16 +1,26 @@
 package com.lsm.frame.controller;
 
+import com.lsm.frame.mapper.CategoryMapper;
+import com.lsm.frame.model.entity.Category;
+import com.lsm.frame.service.intf.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class MainController {
 
+    @Autowired
+    CategoryService categoryService;
+
+
     @RequestMapping("/index")
-    public String hello(Model m) {
-        m.addAttribute("name", "thymeleaf");
+    public String index(Model m) {
+        Category category = new Category();
+        category = categoryService.selectByPrimaryKey(3);
+        System.out.println(",,,"+category);
+        m.addAttribute("category",category);
         return "index";
     }
 }
