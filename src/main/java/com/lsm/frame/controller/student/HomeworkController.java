786c5b00ea8f3021bea6afd5a633b2a0b92e08ff
lsm <1094668269@qq.com>
2020-04-13 13:11:05 +0800

    作业编辑页面

--- src/main/java/com/lsm/frame/controller/student/HomeworkController.java
+++ src/main/java/com/lsm/frame/controller/student/HomeworkController.java
@@ -1,52 +1,36 @@
 package com.lsm.frame.controller.student;
 
-
 import com.lsm.frame.model.entity.User;
-
 import com.lsm.frame.utils.ShiroUtils;
 import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author lsm
  */
 @Controller
-@RequestMapping("/student/course")
-public class CourseController {
+@RequestMapping("/student/homework")
+public class HomeworkController {
 
     private Logger logger =  LoggerFactory.getLogger(this.getClass());
 
     /**
-     * 我学的课页面
+     * 编辑作业页面
      * @param m
      * @return
      */
     @RequiresRoles("student")
     //@RequiresPermissions("system:student)
-    @RequestMapping("/myCourse")
-    public String myCourse(Model m) {
+    @RequestMapping("/edit")
+    public String edit(Model m) {
 
         User user = ShiroUtils.getUser();
         m.addAttribute("user",user);
-        return "student/course/myCourse";
+        return "student/homework/edit";
     }
-
-    @RequiresRoles("student")
-    //@RequiresPermissions("system:student)
-    @RequestMapping("/courseManagement")
-    public String courseManagement(Model m) {
-
-        User user = ShiroUtils.getUser();
-        m.addAttribute("user",user);
-        return "student/course/courseManagement";
-    }
-
-
 
 }
-
